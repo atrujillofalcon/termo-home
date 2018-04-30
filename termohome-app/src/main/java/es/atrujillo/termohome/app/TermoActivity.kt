21ff440e928c54c26b6f66d9439b881362cf9943
atrujillofalcon <atrujillo92work@gmail.com>
2018-04-30 20:08:21 +0200

    Creating termohome-app module

---
+++ termohome-app/src/main/java/es/atrujillo/termohome/app/TermoActivity.kt
@@ -0,0 +1,12 @@
+package es.atrujillo.termohome.app
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TermoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_termo)
+    }
+}
